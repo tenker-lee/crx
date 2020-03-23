e9f8ad316d6989592688996f8ccc39ea7a6c0a77
tenker <gxnulbj@126.com>
2020-03-23 23:26:03 +0800

    555

--- content_scripts.js
+++ content_scripts.js
@@ -1,48 +1,52 @@
 $(document).ready(function () {
-    console.log('content_script.js load.');   
+    console.log('js load');   
     //$('#kw').val("value");
 
     var $by1 = $('.prd-buy');
-    console.log(".prd-buy : "+$by1.length);
+    console.log("prd-buy : "+$by1.length);
     if($by1.length>0){
         $by1[0].click();
     }        
 
     var $su1 = $('.orderinfo-submit');
-    console.log('.orderinfo-submit : '+$su1.length);
+    console.log('orderinfo-submit : '+$su1.length);
 
     if($su1.length>0){
-        var $needPropt = $('.check-txt');
-        console.log("check-txt:" + $needPropt.length);
+
+        var $hongbao = $(".need-hongbao-checkable");
+        console.log("hongbao : " + $hongbao.length);
+
+        if($hongbao.length>0){
+            console.log("hongbao click");
+            setTimeout(() => {
+                $hongbao[0].click();
+            }, 100);
+            
+        }
+
+        var $needPropt = $('.youhui-count');
+        console.log("youhui-count : " + $needPropt.length);
         //console.log($needPropt.text());   
-        $needPropt.each(function (indexInArray) { 
-            var ctext = $(this).text();
-            console.log(indexInArray + ":" +  ctext);
-            if(ctext.indexOf("优惠")>=0)
-            {
-                $(this).click();
-                //$(this).trigger("myclick");        
-                setTimeout(() => {
-                    // $(this).trigger("myclick");
-                    console.log("优惠点击:"+$(".coupon-list .coupon-item").length);
-                    if($(".coupon-list .coupon-item").length>0){
-                        $(".coupon-list .coupon-item")[0].click();
-                            setTimeout(()=>{
-                                console.log("提交")
-                                $('.orderinfo-submit')[0].click();
-                            },10);
-                    }
-                    else{
-                       // window.location.reload();
-                    }
-                }, 200);  
-                return false;                
-            } 
-        });         
+        if($needPropt.length>0){
+            console.log("youhui clieck");
+            $needPropt[0].click();
+            setTimeout(() => {
+                var $coupon = $(".coupon-list>.coupon-item");
+                console.log("coupon : " + $coupon.length);
+                if($coupon.length>0){
+                    $coupon[0].click();             
+                        setTimeout(()=>{
+                            console.log("提交")
+                            
+                            //$('.orderinfo-submit')[0].click();
+                            //$su1[0].click();
+                        },200);                            
+                }
+            }, 200);           
+        }       
     }   
     //刷新   
-    if($by1.length==0 && $su1.length==0)
-    {
+    if($by1.length==0 && $su1.length==0){
         setTimeout(() => {
             window.location.reload();
          }, 1000);   
